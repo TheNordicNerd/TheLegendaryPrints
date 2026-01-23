5570f02d4cd12d2af4ed575aa98ef3d6c8e8a9ec
Adam DeLeon <adam@thenordicnerd.com>
2026-01-23 14:47:16 -0500

    fix toml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/tailwind.css"],
+  // Use static generation for Netlify
+  ssr: true,
+  nitro: {
+    preset: 'netlify',
+  },
   modules: [
     "@nuxt/a11y",
     "@nuxt/eslint",
