d0189d57f917252c545f0014cb40bbfcdc295875
Adam DeLeon <adam@thenordicnerd.com>
2026-02-04 19:41:07 -0500

    update toml configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,10 @@ export default defineNuxtConfig({
   ssr: true,
   nitro: {
     preset: "netlify",
+    output: {
+      dir: ".netlify",
+      publicDir: ".netlify/dist",
+    },
     prerender: {
       crawlLinks: false,
       routes: ["/", "/about", "/contact", "/products", "/cart"],
